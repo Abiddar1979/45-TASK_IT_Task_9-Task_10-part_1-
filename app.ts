0dbb8a3d06d5a1ee8e624465b5a9d435b684b5ff
Abid Dar <abiddar1979@gmail.com>
2024-04-13 16:33:22 +0500

    files added

---
+++ app.ts
@@ -0,0 +1,12 @@
+/* Task # 9: Favorite Number: Store your Favorite number in a variable.Then, using that variable ,create a massage that reveals your favorite number . Print that massage
+
+Task # 10 (Part 1):Adding Comments: Choose two of the programs you’ve written, and add at least one comment to each. If you don’t have anything specific to write because your programs are too simple at this point, just add your name and the current date at the top of each program file. Then write one sentence describing what the program does.*/
+
+// ABID DAR
+//monday , feb-14-2024
+
+// store my favorite number in a variable
+const favorite_Number: number = 99;
+//revealing my favorite number
+const Message: string =( ` My favorite number is , ${favorite_Number}`);
+console.log(Message);
